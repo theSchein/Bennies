7809bb2b8be175ea6b99fe0758de22f5312d95a2
dov <benscheinberg@gmail.com>
2023-09-04 11:50:11 -0400

    added notes

--- shuk/src/pages/index.js
+++ shuk/src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
 
-    <h1>WAGMI</h1>
+    <h1>Ben's cute lil app</h1>
     <Link href="/signin"> 
     Login
     </Link>
@@ -14,6 +14,22 @@ export default function Home() {
     <div> Search</div>
 
     <Search/>
+
+    <h2>About</h2>
+    <p> This is a cute lil app that I made, the goal is for this repo to mature into the IMbD of NFTs</p>
+
+    <h2>Things that work</h2>
+    <p> - Authentication</p>
+    <p> - Connecting your wallet and collecting nft data</p>
+    <p> - Creating an artist page (if you deployed an nft)</p>
+    <p> - NFT pages</p>
+
+    <h2>Things that don't work</h2>
+    <p> - The UI is nonexistant (clearly)</p>
+    <p> - Comments are recorded but not displayed</p>
+    <p> - No real links to artist pages/nft pages</p>
+    <p> - No fuzzy search functionality</p>
+    <p> - everything else.... </p>
 
     
     </>
