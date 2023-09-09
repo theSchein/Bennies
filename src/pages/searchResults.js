44ef099e4cdcb0ed2e55f1ee5faf702a282a86a9
dov <benscheinberg@gmail.com>
2023-09-09 15:07:14 -0400

    added search link functionality

--- src/pages/searchResults.js
+++ src/pages/searchResults.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation'
+import Link from 'next/link';
 
 
 const SearchResults = () => {
@@ -24,24 +25,34 @@ const SearchResults = () => {
             }
         };
 
-        console.log('fetching results', data);
 
         fetchResults();
     }, [query]);
 
-
     return (
+
         <div>
             <h1>Search: {query}</h1>
             {error && <div>Error: {error}</div>}
             <h2>NFTs</h2>
             {data.nfts.map((nft) => (
+                <Link href={`/nft/${nft.nft_id}/${nft.nft_name}`}>
                 <div key={nft.nft_id}>
                     <h3>{nft.nft_name}</h3>
                     <p>Contract Address: {nft.contract_address}</p>
                     <p>Deployer Address: {nft.deployer_address}</p>
                     <p>Description: {nft.nft_description}</p>
                 </div>
+                </Link>
+            ))}
+            <h2>Artists</h2>
+            {data.artists.map((artists) => (
+                <Link href={`/artist/${artists.artist_id}/${artists.artist_name}`}>
+                <div key={artists.artist_id}>
+                    <h3>{artists.artist_name}</h3>
+                    <p>Description: {artists.nft_description}</p>
+                </div>
+                </Link>
             ))}
         </div>
     );}
