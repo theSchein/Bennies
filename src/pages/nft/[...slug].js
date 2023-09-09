8dc2270760b7fef95efe6b3bccf7f590c6f69ffb
dov <benscheinberg@gmail.com>
2023-09-08 21:16:29 -0400

    added catchall route to nfts. nfts grabbed by id and names just displayed for seo

--- src/pages/nft/[...slug].js
+++ src/pages/nft/[...slug].js
@@ -1,8 +1,22 @@
-// pages/nft/[slug].js
+// pages/nft/[...slug].js
 import db from '../../lib/db';
 import CommentSection from '../../components/comment/Comments';
 
+
+export async function getServerSideProps({ params }) {
+    const { slug } = params;
+    // Fetch the NFT data based on the slug from your database/API
+    const nft = await db.one('SELECT * FROM nfts WHERE nft_id = $1', [slug[0]]);
+
+    console.log('slug:' + slug[1]);
+
+    return { props: { nft } };
+}
+
+
 export default function NftPage({ nft }) {
+    
+
     return (
         <div>
             <h1>{nft.nft_name}</h1>
@@ -13,11 +27,8 @@ export default function NftPage({ nft }) {
     );
 }
 
-export async function getServerSideProps({ params }) {
-    const { slug } = params;
-    // Fetch the NFT data based on the slug from your database/API
-    const nft = await db.one('SELECT * FROM nfts WHERE nft_name = $1', [slug]);
-    return { props: { nft } };
-}
+
+
+
 
 
