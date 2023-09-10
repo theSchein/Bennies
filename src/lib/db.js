d11a615c3eba406a30802a4e7a2b97711f4e94c4
dov <benscheinberg@gmail.com>
2023-09-10 12:44:58 -0400

    added db integration for vercel cloud postgres

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,11 +4,11 @@ import pgPromise from 'pg-promise';
 const pgp = pgPromise({});
 
 const db = pgp({
-  host: 'localhost',
-  port: 5432,
-  database: 'postgres',
-  user: 'myuser',
-  password: 'mypassword'
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  database: process.env.POSTGRES_DATABASE || 'postgres',
+  user: process.env.POSTGRES_USER || 'myuser',
+  password: process.env.POSTGRES_PASSWORD || 'mypassword'
 });
 
 export default db;
