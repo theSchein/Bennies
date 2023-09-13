0820575dd9fd868cb06cc49c4160fa077dd97b84
dov <benscheinberg@gmail.com>
2023-09-13 07:50:51 -0400

    not requiring ssl, hopefully makes the database work

--- src/lib/db.js
+++ src/lib/db.js
@@ -8,8 +8,11 @@ const db = pgp({
   port: parseInt(process.env.DB_PORT, 10) || 5432,
   database: process.env.POSTGRES_DATABASE || 'postgres',
   user: process.env.POSTGRES_USER || 'myuser',
-  password: process.env.POSTGRES_PASSWORD || 'mypassword'
-});
+  password: process.env.POSTGRES_PASSWORD || 'mypassword',
+  ssl: {
+    rejectUnauthorized: false
+  },
+})
 
 export default db;
 
