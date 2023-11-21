bb83d1286f1e047dc75565081e49a75be291912e
dov <benscheinberg@gmail.com>
2023-11-21 11:02:35 -0500

    updated padding to make it look a little better

--- src/pages/pitchdeck.js
+++ src/pages/pitchdeck.js
@@ -1,10 +1,11 @@
 const PitchDeck= () => {
     return (
-        <div>
+        <div className="aspect-w-16 aspect-h-9 overflow-auto">
             <iframe 
+                className="aspect-content  border-none"
                 src="pitchdeck.pdf" 
                 width="100%" 
-                height="1000px"
+                height="1000"
                 style={{ border: 'none' }}>
             </iframe>
         </div>
