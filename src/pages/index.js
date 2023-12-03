9d108fe6ffabb1c1a1e9d629a867bf59e2150bcd
josephmartinmoran <josephmmoran11@gmail.com>
2023-12-03 13:56:29 -0500

    waitlist component and .api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 // pages/index.js
 // This is the homepage for the App and is the first page that is loaded when the app is opened.
 
+import WaitlistForm from "@/components/WaitlistForm";
+
 export default function Home() {
     return (
 <div className="min-h-screen flex flex-col items-center justify-center p-2">
@@ -18,16 +20,8 @@ export default function Home() {
             Discovry.xyz shows you the utility and rights of each NFT and incentivizes active engagement between the creators and owners.
         </p>
     </div>
-
-    <div className="mt-10 bg-primary p-4 sm:p-6 lg:p-8 rounded-2xl shadow-xl max-w-md w-full">
-        <h3 className="font-subheading text-xl sm:text-2xl lg:text-2xl mb-4 text-secondary text-center">
-            Join Waitlist for Beta
-        </h3>
-        <input className="w-full p-2 sm:p-3 mb-3 border-2 border-cyan-500 rounded-lg" type="text" placeholder="Name" />
-        <input className="w-full p-2 sm:p-3 mb-3 border-2 border-cyan-500 rounded-lg" type="text" placeholder="Email" />
-        <button className="w-full py-2 sm:py-3 bg-gradient-to-r from-cyan-600 to-yellow-950 text-white rounded-lg font-bold hover:from-yellow-600 hover:to-cyan-700 transition duration-300">
-            Submit
-        </button>
+    <div>
+        <WaitlistForm />
     </div>
 </div>
     );
