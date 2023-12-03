a1c86d68246acc6a4e1777d7fee4695793d12bb7
josephmartinmoran <josephmmoran11@gmail.com>
2023-12-03 14:24:54 -0500

    postgres waitlist db set up on vercel test worked

--- src/pages/api/waitlist.js
+++ src/pages/api/waitlist.js
@@ -1,13 +1,27 @@
 // pages/api/waitlist.js
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    connectionString: process.env.waitlist_url, // Use environment variable for the database URL
+    ssl: {
+        rejectUnauthorized: false // This is important for some hosted databases like Heroku
+    }
+});
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { name, email } = req.body;
 
-        // Add your database connection and insertion logic here
-        // For example, inserting into a MongoDB database
+        try {
+            // Insert data into the database
+            const query = 'INSERT INTO waitlist (name, email) VALUES ($1, $2)';
+            await pool.query(query, [name, email]);
 
-        res.status(200).json({ message: 'Added to waitlist!' });
+            res.status(200).json({ message: 'Added to waitlist!' });
+        } catch (error) {
+            console.error('Database Error:', error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
     } else {
         // Handle any other HTTP methods
         res.setHeader('Allow', ['POST']);
