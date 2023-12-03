329a64ac031d14b216bcf30bb5bbf74cb747cf11
dov <benscheinberg@gmail.com>
2023-12-03 10:23:35 -0500

    footer reworking

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,18 +2,29 @@
 // Footer style component. There is no logic to actually handle anything here.
 // The footer should be revisted and updated to be more useful.
 
-import React from "react"
+import React from 'react';
+import Link from 'next/link'
 
 function Footer() {
-  const year = new Date().getFullYear()
-  
-  return (
-    <footer>
-      <p className="text-center font-bold">Copyright ⓒ {year}</p>
-      <p className="text-center font-bold">All Rights Reserved By Discovry</p>
-    </footer>
-  )
+    return (
+<footer className="bg-teal-900 text-white py-8">
+    <div className="container mx-auto px-4">
+        <div className="footer-content flex flex-col sm:flex-row justify-between items-center">
+            <div className="footer-section mb-4 sm:mb-0">
+                <Link href="/team" legacyBehavior>
+                    <a className="text-lg font-medium hover:text-gray-300 transition-colors duration-300">Our Team</a>
+                </Link>
+            </div>
+            <div className="footer-section text-center sm:text-left">
+                <h4 className="text-xl font-semibold mb-2">Resources</h4>
+                <p className="mb-4">Download our pitch deck to get a detailed overview of our project.</p>
+                <a href="pitchdeck.pdf" download="pitchdeck.pdf" className="text-blue-400 hover:text-blue-500 transition-colors duration-300">Download Pitch Deck</a>
+            </div>
+        </div>
+    </div>
+</footer>
+    );
 }
 
-export default Footer
+export default Footer;
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -30,9 +30,5 @@ export default function Home() {
         </button>
     </div>
 </div>
-
-
-
-
     );
 }
