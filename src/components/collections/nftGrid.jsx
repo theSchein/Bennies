eeb60da62ada79301b2ba7127fa0c33b7fd86541
dov <benscheinberg@gmail.com>
2023-12-31 10:21:04 -0500

    minimum functionality of the page, could use another feature or two before merging

---
+++ src/components/collections/nftGrid.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+
+const NftGrid = ({ nftData }) => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid
+                container
+                spacing={{ xs: 2, md: 3 }}
+                columns={{ xs: 4, sm: 8, md: 12 }}
+            >
+                {nftData.map((nft, index) => (
+                    <Link
+                        href={`/nft/${nft.nft_id}/${nft.nft_name}`}
+                        passHref
+                        legacyBehavior
+                    >
+                        <Grid xs={2} sm={4} md={4} key={index}>
+                            {/* Render NFT details here */}
+                            <Image
+                                src={nft.media_url}
+                                alt={nft.nft_name}
+                                width={200}
+                                height={200}
+                            />
+                            <p>{nft.nft_name}</p>
+                        </Grid>
+                    </Link>
+                ))}
+            </Grid>
+        </Box>
+    );
+};
+
+export default NftGrid;
