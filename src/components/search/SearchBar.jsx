66721b25aa189f09c3d82e8beb7d18b3ec4dde68
dov <benscheinberg@gmail.com>
2024-01-23 16:59:02 -0500

    major facelift to search page and had the search bar component use the hook

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -2,67 +2,13 @@
 // This component handles the logic for the search bar. On the Search Page
 
 import * as React from "react";
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import useSearch from "../hooks/useSearch";
+
 
 function SearchBar() {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [suggestions, setSuggestions] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const router = useRouter();
+    const { searchTerm, setSearchTerm, handleSubmit } = useSearch();
 
-    useEffect(() => {
-        if (searchTerm.trim() === "") {
-            setSuggestions([]);
-            return;
-        }
 
-        const fetchSuggestions = async () => {
-            setLoading(true);
-            setError(null);
-
-            try {
-                const response = await fetch(
-                    `/api/search?query=${encodeURIComponent(searchTerm)}`,
-                );
-                const data = await response.json();
-                setSuggestions(data.results || []);
-            } catch (err) {
-                setError("Failed to fetch suggestions");
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchSuggestions();
-    }, [searchTerm]);
-
-    const handleClick = (e) => {
-        e.preventDefault();
-        router.push("/search?query=" + searchTerm);
-    };
-
-    return (
-        <Bar
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-            suggestions={suggestions}
-            loading={loading}
-            error={error}
-            handleClick={handleClick}
-        />
-    );
-}
-
-function Bar({
-    searchTerm,
-    setSearchTerm,
-    suggestions,
-    loading,
-    error,
-    handleClick,
-}) {
     return (
         <div className="flex flex-col space-y-4">
             <div className="flex">
@@ -74,20 +20,12 @@ function Bar({
                     className="p-2 border rounded w-full"
                 />
                 <button
-                    onClick={handleClick}
-                    className="p-2 text-white rounded hover:bg-opacity-80 ml-2 bg-gradient-to-r from-gray-300 via-gray-400 to-gray-500" // Gray gradient styling
+                    onClick={handleSubmit}
+                    className="p-3 ml-2 text-light-quaternary dark:text-dark-quaternary bg-light-secondary dark:bg-dark-primary rounded-full transition ease-in duration-200 hover:bg-light-quaternary dark:hover:bg-dark-tertiary hover:text-light-secondary dark:hover:text-dark-secondary " // Gray gradient styling
                 >
                     Submit
                 </button>
             </div>
-            {error && <div className="text-secondary">{error}</div>}
-            <ul className="w-full space-y-2">
-                {suggestions.map((suggestion) => (
-                    <li key={suggestion.id} className="p-2 border-b">
-                        {suggestion.name}
-                    </li>
-                ))}
-            </ul>
         </div>
     );
 }
