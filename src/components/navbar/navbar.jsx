f5a430afb44e333d363aeb9fe5b1682801f2b0e6
dov <benscheinberg@gmail.com>
2024-01-28 09:58:03 -0500

    got build to work and made an insignificant commit to see wtf is going on

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -9,9 +9,9 @@ import Toolbar from "@mui/material/Toolbar";
 import DiamondIcon from "@mui/icons-material/Diamond";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu"; // Import the MenuIcon for the hamburger menu
+import MenuIcon from "@mui/icons-material/Menu"; 
 import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem"; // Import MenuItem
+import MenuItem from "@mui/material/MenuItem"; 
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import { useSession } from "next-auth/react";
