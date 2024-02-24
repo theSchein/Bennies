da0b7aea98fbee7829a4a1a65ffcabf5d650f784
dov <benscheinberg@gmail.com>
2024-02-23 20:56:42 -0500

    made the nft grid look much better

--- src/components/collections/nftGrid.jsx
+++ src/components/collections/nftGrid.jsx
@@ -25,22 +25,21 @@ const NftGrid = ({ nftData, query }) => {
                             item
                             xs={2}
                             sm={4}
-                            md={4}
+                            md={3} // Adjust based on desired layout
                             sx={{
                                 display: "flex",
                                 flexDirection: "column",
                                 alignItems: "center",
-                                padding: 1,
+                                p: { xs: 1, md: 2 }, // Adjust padding
+                                m: 1, // Adjust margin
                             }}
-                            className="transition duration-300 bg-light-primary dark:bg-dark-secondary m-1 ease-in-out transform hover:scale-120 hover:shadow-lg rounded-lg"
+                            className="transition duration-300 ease-in-out transform hover:scale-120 hover:shadow-lg rounded-lg"
                         >
                             <Box
                                 sx={{
                                     width: "100%",
-                                    height: 400,
+                                    pt: "125%", // Aspect ratio hack (height: 0, paddingBottom: percentage of width)
                                     position: "relative",
-                                    marginBottom: 1,
-                                    color: "blue",
                                 }}
                             >
                                 <Image
@@ -51,18 +50,18 @@ const NftGrid = ({ nftData, query }) => {
                                     }
                                     alt={nft.nft_name}
                                     layout="fill"
-                                    objectFit="contain" // Changed from "cover" to "contain"
+                                    objectFit="contain"
                                 />
                             </Box>
-                            <div className="text-dark-quaternary font-bold">
-                                <p style={{ textAlign: "center", marginTop: 0 }}>
-                                    {nft.nft_name}
-                                </p>
-                                <div className="flex-wrap">
-                                    <Likes nft_id={nft.nft_id} />
-                                    <div className="">
-                                        <CommentIcon />
-                                        {nft.comment_count}
+                            <div className="bg-light-primary dark:bg-dark-secondary rounded-md p-2 w-full text-center text-dark-quaternary font-bold">
+                                <p className="mb-3">{nft.nft_name}</p>
+                                <div className="flex justify-between items-center">
+                                    <div className="flex items-center">
+                                        <Likes nft_id={nft.nft_id} />
+                                    </div>
+                                    <div className="flex items-center">
+                                        <CommentIcon className="mr-2" />
+                                        <span>{nft.comment_count}</span>
                                     </div>
                                 </div>
                             </div>
