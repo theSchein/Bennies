a484e97f102882b51987199bc9e9cdd1f30085ab
dov <benscheinberg@gmail.com>
2024-05-14 08:02:21 -0400

    first pass at updating colors fonts and favicon

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,13 @@ export default function About() {
         <div className="min-h-screen bg-gradient-light dark:bg-gradient-dark flex flex-col items-center justify-center p-4 text-light-quaternary dark:text-dark-quaternary">
             <div className="bg-light-secondary dark:bg-dark-secondary bg-opacity-90 py-10 px-6 sm:px-10 lg:px-12 rounded-2xl shadow-xl max-w-4xl mb-10 space-y-8">
                 <h1 className="font-heading text-5xl sm:text-6xl lg:text-7xl text-center leading-tight mb-8">
-                    DISCOVRY.XYZ
+                    Bennies
                 </h1>
                 <p className="text-xl sm:text-2xl text-center">
                     Finding out what your NFT does is hard, and I want to make it easy.
                     Marketplaces only show you how much they cost and not what to do with them.
+                    When a new holder buys an NFT they often do not know everything they can do with them or know much about the 
+                    community that they have joined. 
                 </p>
                 <p className="text-xl sm:text-2xl text-center mt-4">
                     Discovry.xyz shows you not just what people think of your NFT,
