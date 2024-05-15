5ca28af7ed8fd73ef264794ce63f2de6c5f56208
dov <benscheinberg@gmail.com>
2024-05-15 12:23:09 -0400

    added postmark styled templates instead of basic text

--- src/lib/emailUtils.js
+++ src/lib/emailUtils.js
@@ -2,13 +2,14 @@
 import client from "@/lib/postmarkClient";  
 
 export const sendVerificationEmail = async (email, link) => {
-    await client.sendEmail({
-        From: "ben@discovry.xyz",
+    await client.sendEmailWithTemplate({
+        From: "ben@bennies.fun",
         To: email,
-        Subject: "Verify Your Email Address",
-        TextBody: `Please verify your email by clicking on this link: ${link}`,
-        HtmlBody: `<strong>Please verify your email</strong> by clicking on this link: <a href="${link}">Verify Email</a>`,
-        MessageStream: "outbound"
+        TemplateAlias: "verify",
+        TemplateModel: {
+            action_url: link,
+            support_email: "ben@bennies.fun"
+        }
     });
 };
 
@@ -16,12 +17,14 @@ export const sendWelcomeEmail = async (email, username) => {
     const baseUrl = process.env.BASE_URL;
     const welcomeLink = `${baseUrl}/`; 
 
-    await client.sendEmail({
-        From: "ben@discovry.xyz",
+    await client.sendEmailWithTemplate({
+        From: "ben@bennies.fun",
         To: email,
-        Subject: "Welcome to Discovry!",
-        TextBody: `Hi ${username}, welcome to Discovry! Start exploring now: ${welcomeLink}`,
-        HtmlBody: `<strong>Hi ${username},</strong><p>Welcome to Discovry!</p><p>SI am so happy to have you. Start exploring now: <a href="${welcomeLink}">Explore</a></p>`,
-        MessageStream: "outbound"
+        TemplateAlias: "welcome",
+        "TemplateModel": {
+            name: username,
+            action_url: welcomeLink,
+            support_email: "ben@bennies.fun"
+          }
     });
 };
