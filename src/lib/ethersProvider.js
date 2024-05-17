8670ae514afd373ab41f1af234d217266b6d0760
dov <benscheinberg@gmail.com>
2024-05-17 19:20:02 -0400

    temp fix i hate that i dont know how to deploy without pushing but here we are

--- src/lib/ethersProvider.js
+++ src/lib/ethersProvider.js
@@ -3,6 +3,13 @@
 
 import Web3 from 'web3';
 
+const infuraApiKey = process.env.INFURA_API_KEY;
+
+if (!infuraApiKey) {
+    throw new Error("INFURA_API_KEY is not set in environment variables.");
+}
+
+console.log("Using Infura API Key:", infuraApiKey);
 // https://rpc.dexosphere.xyz/
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`)); //https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
 // const web3 = new Web3(new Web3.providers.HttpProvider(`https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
