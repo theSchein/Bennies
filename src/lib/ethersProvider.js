4d4044233d76b5ca988612dddf9ecec8c85b4616
dov <benscheinberg@gmail.com>
2024-05-17 21:23:36 -0400

    delete this later

--- src/lib/ethersProvider.js
+++ src/lib/ethersProvider.js
@@ -5,10 +5,8 @@ import Web3 from 'web3';
 
 const infuraApiKey = process.env.INFURA_API_KEY;
 
-
 // https://rpc.dexosphere.xyz/
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`));
-//`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`)); //https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/81b0c120b79f481299158c5718b96e17`)); //https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
 // const web3 = new Web3(new Web3.providers.HttpProvider(`https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
 
 
