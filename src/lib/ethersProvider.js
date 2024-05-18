b9e932e941cbf89aebe624a7dda7ae3bcb7f9d1c
dov <benscheinberg@gmail.com>
2024-05-17 20:59:51 -0400

    trying alchemy instead of infura

--- src/lib/ethersProvider.js
+++ src/lib/ethersProvider.js
@@ -5,12 +5,10 @@ import Web3 from 'web3';
 
 const infuraApiKey = process.env.INFURA_API_KEY;
 
-if (!infuraApiKey) {
-    throw new Error("INFURA_API_KEY is not set in environment variables.");
-}
 
 // https://rpc.dexosphere.xyz/
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`)); //https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`));
+//`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`)); //https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
 // const web3 = new Web3(new Web3.providers.HttpProvider(`https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
 
 
