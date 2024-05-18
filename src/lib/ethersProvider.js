a0627681147a0da2a2e14c76821ab5bd127297c3
dov <benscheinberg@gmail.com>
2024-05-18 07:43:03 -0400

    drpc logging and test

--- src/lib/ethersProvider.js
+++ src/lib/ethersProvider.js
@@ -3,11 +3,20 @@
 
 import Web3 from 'web3';
 
-console.log('drpc key:', process.env.DRPC_API_KEY)
+const drpcApiKey = process.env.DRPC_API_KEY;
 
-// https://rpc.dexosphere.xyz/
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://lb.drpc.org/ogrpc?network=ethereum&dkey=${process.env.DRPC_API_KEY}`)); //https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
-// const web3 = new Web3(new Web3.providers.HttpProvider(`https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
+if (!drpcApiKey) {
+    throw new Error("DRPC_API_KEY is not set in environment variables.");
+}
 
+const provider = `https://lb.drpc.org/ogrpc?network=ethereum&dkey=${drpcApiKey}`;
+console.log("Using DRPC Provider URL:", provider);
+
+const web3Provider = new Web3.providers.HttpProvider(provider);
+const web3 = new Web3(web3Provider);
+
+web3.eth.net.isListening()
+    .then(() => console.log('Web3 is connected'))
+    .catch(e => console.log('Something went wrong', e));
 
 export default web3;
