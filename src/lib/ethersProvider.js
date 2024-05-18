b5c200fbec236a5c8a72c6ea7f9320bc303acfe8
dov <benscheinberg@gmail.com>
2024-05-17 20:41:55 -0400

    clean up to try to live and see if that fixes things

--- src/lib/ethersProvider.js
+++ src/lib/ethersProvider.js
@@ -4,13 +4,11 @@
 import Web3 from 'web3';
 
 const infuraApiKey = process.env.INFURA_API_KEY;
-console.log(infuraApiKey)
 
 if (!infuraApiKey) {
     throw new Error("INFURA_API_KEY is not set in environment variables.");
 }
 
-console.log("Using Infura API Key:", infuraApiKey);
 // https://rpc.dexosphere.xyz/
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`)); //https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
 // const web3 = new Web3(new Web3.providers.HttpProvider(`https://rough-spring-frog.quiknode.pro/${process.env.QUIKNODE_API_KEY}`));
