8879df36b34563ac99033dc11d3974423990ebc7
dov <benscheinberg@gmail.com>
2024-05-29 10:05:16 -0400

    added structure for execute

---
+++ database-toolkit/actions/execute/__init__.py
@@ -0,0 +1,10 @@
+# actions/execute/execute.py
+
+
+
+
+def execute():
+    print("Starting the staging process...")
+
+if __name__ == '__main__':
+    execute()
--- database-toolkit/main.py
+++ database-toolkit/main.py
@@ -2,12 +2,14 @@
 from actions.staging.staging import staging
 from actions.cleanup.cleanup import cleanup
 from actions.backup.backup import backup
+from actions.execute.execute import execute
 
 def main_menu():
     print("Welcome to the Data Management Toolkit CLI")
     print("1. Add to Staging environment")
     print("2. Clean up Prod Database")
     print("3. Back Up Database")
+    print("4. Add to Prod Database")
     print("0. Exit")
     choice = input("Enter your choice: ")
 
@@ -17,6 +19,8 @@ def main_menu():
         cleanup()
     elif choice == '3':
         backup()
+    elif choice == '4':
+        execute()
     elif choice == '0':
         print("Exiting the program.")
         exit()
