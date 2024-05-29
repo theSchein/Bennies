7e4d0ccf341326233764c64c85a4612b47662793
dov <benscheinberg@gmail.com>
2024-05-29 13:17:35 -0400

    fixed up heading tile style, made signup bigger

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,36 +8,37 @@ import SignupButton from "@/components/homepage/signupButton";
 export default function Home() {
     return (
         <div className="min-h-screen bg-gradient-light dark:bg-gradient-dark flex flex-col items-center justify-center p-2 gap-10">
-            <div className="bg-light-secondary dark:bg-dark-secondary bg-opacity-90 py-10 px-6 sm:px-10 lg:px-14 rounded-xl shadow-xl max-w-4xl mb-10 w-full">
+            <div className="bg-light-secondary dark:bg-dark-secondary bg-opacity-90 py-16 px-10 sm:px-20 lg:px-28 rounded-xl shadow-xl w-full max-w-6xl mb-10">
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 items-center">
-                    <div className="flex flex-col items-center sm:items-start">
-                        <div className="flex items-center">
+                    <div className="flex flex-col items-center sm:items-start w-full">
+                        <div className="flex items-center w-full">
                             <Image
                                 src={logo}
                                 alt="Bennies Logo"
-                                width={75}
-                                height={75}
+                                width={100}
+                                height={100}
                                 className="inline-block"
                             />
-                            <h1 className="font-heading text-3xl sm:text-5xl lg:text-6xl text-light-font dark:text-light-ennies leading-tight ml-2">
+                            <h1 className="font-heading text-4xl sm:text-6xl lg:text-7xl text-light-font dark:text-light-ennies leading-tight ml-4">
                                 ENNIES
                             </h1>
                         </div>
-                        <h3 className="italic bold text-light-font dark:text-dark-font mt-4 text-center sm:text-left lg:text-xl sm:text-lg">
+                        <h3 className="italic bold text-light-font dark:text-dark-font mt-5 text-center sm:text-left text-3xl ">
                             Find the Benefits, not Price
                         </h3>
-                        <SignupButton />
+                        <div className="mt-6 w-full flex justify-center sm:justify-start">
+                            <SignupButton className="p-4 sm:p-6 lg:p-8 text-xl sm:text-2xl lg:text-3xl transform scale-200" />
+                        </div>
                     </div>
-                    <div className="text-center sm:text-left bg-light-tertiary dark:bg-dark-tertiary p-5 rounded-xl shadow-xl">
-                        <p className="text-lg text-light-font dark:text-light-ennies">
-                            Bennies collects NFT holder benefits showing you the actual value of these assets.
-                            Register your NFTs to get welcomed by your specific community as a member and find out how to get involved.
+                    <div className="text-center sm:text-left bg-light-tertiary dark:bg-dark-tertiary p-7 rounded-xl shadow-xl">
+                        <p className="text-xl text-light-font dark:text-light-ennies">
+                            Bennies has all the information on what you can do with your onchain stuff.
                         </p>
                     </div>
                 </div>
             </div>
 
-            <div className="bg-light-secondary dark:bg-dark-secondary bg-opacity-90 py-8 px-4 sm:px-10 lg:px-14 rounded-xl shadow-xl max-w-4xl w-full mb-8">
+            <div className="bg-light-secondary dark:bg-dark-secondary bg-opacity-90 py-10 px-6 sm:px-14 lg:px-20 rounded-xl shadow-xl max-w-4xl w-full mb-8">
                 <HowItWorks />
             </div>
 
